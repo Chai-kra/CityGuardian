718100b16830e33f71afcdad3152a78222aec77b
Alice Lee <alicelyh.2006@gmail.com>
2026-08-14 22:04:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main Page</title>
+</head>
+<body>
+    <a href="LogIn.php"><button type="button">Log In</button></a>
+    <a href="Register.php"><button type="button">Register</button></a>
+</body>
