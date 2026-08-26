95472b421ffc4936c182354ee6e4bd5e83744d40
Alice Lee <alicelyh.2006@gmail.com>
2026-08-26 20:15:26 +0800

    Modified db.php

--- db.php
+++ db.php
@@ -3,12 +3,19 @@ require __DIR__ . '/Library/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/Library');
 $dotenv->load();
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "cityguardian";
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'] ?? 3306;
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
